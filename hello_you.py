c31f7bbe37baa396cf18db286c432f9596dff0d0
br305893 <ben.rosner.williamsburg@gmail.com>
2019-12-13 15:15:30 -0500

    finish hello you project

---
+++ hello_you.py
@@ -0,0 +1,19 @@
+# Ask user for name
+
+name = input("What is your name?: ")
+
+# Ask user for age
+
+age = input("How old are you?: ")
+
+# Ask user for city
+
+city = input("What city do you live in?: ")
+
+# Ask user what they enjoy
+
+love = input("What do you love doing?: ")
+
+# Create output text
+
+# Print output to screen
